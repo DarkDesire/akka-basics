8c65507527c94f29b356b042b6b1c4acdcec23f0
Eldar Nezametdinov <oh.eldarkaa@gmail.com>
2018-06-17 16:30:02 +0300

    CounterSpec: handle sequence of messages #2

--- src/test/scala/CounterSpec.scala
+++ src/test/scala/CounterSpec.scala
@@ -21,30 +21,17 @@ class CounterSpec extends TestKit(ActorSystem("test-system"))
     val counter = system.actorOf(Props[Counter])
 
     sender.send(counter, Counter.GetCount)
-
-    val state = sender.expectMsgType[Int]
-
-    state mustEqual 0
+    sender.expectMsgType[Int]
   }
   it should "handle Increment message" in {
     val counter = system.actorOf(Props[Counter])
     counter ! Cmd(Counter.Increment(1))
     expectNoMessage(1.seconds)
   }
-  it should "handle GetCount message" in {
-    val counter = system.actorOf(Props[Counter])
-    counter ! GetCount
-    expectMsg(1)
-  }
   it should "handle Decrement message" in {
     val counter = system.actorOf(Props[Counter])
     counter ! Cmd(Counter.Decrement(1))
     expectNoMessage(1.seconds)
-  }
-  it should "handle GetCount message #2" in {
-    val counter = system.actorOf(Props[Counter])
-    counter ! GetCount
-    expectMsg(0)
   }
   it should "handle sequence of messages" in {
     val sender = TestProbe("testProbe")
@@ -63,16 +50,25 @@ class CounterSpec extends TestKit(ActorSystem("test-system"))
 }
 
 /*
-16:09:24.811 DEBUG persistent.counter.Counter - Starting ...
-16:09:25.512 DEBUG persistent.counter.Counter - Recovery completed and now I'll switch to normal receiving mode
-16:09:26.560 DEBUG persistent.counter.Counter - Starting ...
-16:09:26.614 DEBUG persistent.counter.Counter - Counter: receive Evt(Increment(1)) on recovering mood
-16:09:26.614 DEBUG persistent.counter.Counter - Recovery completed and now I'll switch to normal receiving mode
-16:09:26.620 DEBUG persistent.counter.Counter - Starting ...
-16:09:26.628 DEBUG persistent.counter.Counter - Counter: receive Evt(Increment(1)) on recovering mood
-16:09:26.629 DEBUG persistent.counter.Counter - Recovery completed and now I'll switch to normal receiving mode
-16:09:26.630 DEBUG persistent.counter.Counter - Counter: receive Cmd(Decrement(1))
-16:09:27.642 DEBUG persistent.counter.Counter - Starting ...
-16:09:27.658 DEBUG persistent.counter.Counter - Counter: receive snapshot with data State(0) on recovering mood
-16:09:27.660 DEBUG persistent.counter.Counter - Recovery completed and now I'll switch to normal receiving mode
- */
+16:27:50.762 [test-system-akka.actor.default-dispatcher-3] DEBUG persistent.counter.Counter - Starting ...
+16:27:51.199 [test-system-akka.actor.default-dispatcher-7] DEBUG persistent.counter.Counter - Counter: receive snapshot with data State(5) on recovering mood
+16:27:51.528 [test-system-akka.actor.default-dispatcher-3] DEBUG persistent.counter.Counter - Counter: receive Evt(Increment(4)) on recovering mood
+16:27:51.528 [test-system-akka.actor.default-dispatcher-3] DEBUG persistent.counter.Counter - Counter: receive Evt(Decrement(3)) on recovering mood
+16:27:51.529 [test-system-akka.actor.default-dispatcher-3] DEBUG persistent.counter.Counter - Counter: receive Evt(Increment(1)) on recovering mood
+16:27:51.530 [test-system-akka.actor.default-dispatcher-3] DEBUG persistent.counter.Counter - Counter: receive Evt(Decrement(1)) on recovering mood
+16:27:51.530 [test-system-akka.actor.default-dispatcher-3] DEBUG persistent.counter.Counter - Counter: receive Evt(Increment(2)) on recovering mood
+16:27:51.531 [test-system-akka.actor.default-dispatcher-3] DEBUG persistent.counter.Counter - Counter: receive Evt(Increment(4)) on recovering mood
+16:27:51.532 [test-system-akka.actor.default-dispatcher-3] DEBUG persistent.counter.Counter - Counter: receive Evt(Decrement(3)) on recovering mood
+16:27:51.533 [test-system-akka.actor.default-dispatcher-3] DEBUG persistent.counter.Counter - Recovery completed and now I'll switch to normal receiving mode
+16:27:52.568 [test-system-akka.actor.default-dispatcher-15] DEBUG persistent.counter.Counter - Starting ...
+16:27:52.582 [test-system-akka.actor.default-dispatcher-14] DEBUG persistent.counter.Counter - Counter: receive snapshot with data State(10) on recovering mood
+16:27:52.584 [test-system-akka.actor.default-dispatcher-14] DEBUG persistent.counter.Counter - Recovery completed and now I'll switch to normal receiving mode
+16:27:52.584 [test-system-akka.actor.default-dispatcher-14] DEBUG persistent.counter.Counter - Counter: receive Cmd(Decrement(1))
+16:27:53.583 [test-system-akka.actor.default-dispatcher-13] DEBUG persistent.counter.Counter - Starting ...
+16:27:53.589 [test-system-akka.actor.default-dispatcher-12] DEBUG persistent.counter.Counter - Counter: receive snapshot with data State(10) on recovering mood
+16:27:53.594 [test-system-akka.actor.default-dispatcher-13] DEBUG persistent.counter.Counter - Counter: receive Evt(Decrement(1)) on recovering mood
+16:27:53.595 [test-system-akka.actor.default-dispatcher-13] DEBUG persistent.counter.Counter - Recovery completed and now I'll switch to normal receiving mode
+16:27:53.597 [test-system-akka.actor.default-dispatcher-13] DEBUG persistent.counter.Counter - Counter: receive Cmd(Increment(2))
+16:27:53.603 [test-system-akka.actor.default-dispatcher-12] DEBUG persistent.counter.Counter - Counter: receive Cmd(Increment(4))
+16:27:53.605 [test-system-akka.actor.default-dispatcher-12] DEBUG persistent.counter.Counter - Counter: receive Cmd(Decrement(3))
+*/
